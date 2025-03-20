15af6932b35fbb503849f4986ccc06e9ae824ff1
Shin <pro100pro20021@mail.ru>
2025-03-20 14:57:58 +1000

    eqwqwe

--- backend/src/database.module.ts
+++ backend/src/database.module.ts
@@ -2,8 +2,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Film } from './films/entity'; // Убедитесь, что путь правильный
-import { Schedule } from './films/entity'; // Убедитесь, что путь правильный
+import { Film } from './films/entity';
+import { Schedule } from './films/entity';
 
 @Module({
   imports: [
@@ -12,18 +12,16 @@ import { Schedule } from './films/entity'; // Убедитесь, что пут�
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          type: 'postgres',
-          host: configService.get<string>('DATABASE_HOST'),
-          port: configService.get<number>('DATABASE_PORT'),
-          username: configService.get<string>('DATABASE_USERNAME'),
-          password: configService.get<string>('DATABASE_PASSWORD'),
-          database: configService.get<string>('DATABASE_NAME'),
-          entities: [Film, Schedule],
-          synchronize: process.env.NODE_ENV !== 'production', // Синхронизация только в разработке
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Film, Schedule],
+        synchronize: false, 
+      }),
       inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Film, Schedule]), 
